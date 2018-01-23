4c18f2ae45d821d8a27cb4afb342371609a4c23a
Yong Wang <yongwangd@gmail.com>
2018-01-23 13:40:30 -0500

    3

--- app/utils/getLocalFiles.js
+++ app/utils/getLocalFiles.js
@@ -7,13 +7,20 @@ const { dialog } = remote;
 const getLocalFile = options =>
   new Promise(resolve => dialog.showOpenDialog(options, r => resolve(r)));
 
-export const singleFilePath = options => getLocalFile(options).then(files => files[0]);
+export const singleFilePath = options =>
+  getLocalFile(options).then(files => files[0]);
 export const filePaths = getLocalFile;
 
 export const localAudioPaths = (options = {}) => {
   const newOpts = R.compose(
-    R.over(R.lensProp('filters'), R.append({ name: 'Audios', extensions: ['mp3'] })),
-    R.over(R.lensProp('properties'), R.pipe(R.append('openFile'), R.append('multiSelections'))),
+    R.over(
+      R.lensProp('filters'),
+      R.append({ name: 'Audios', extensions: ['mp3', 'mp4', 'avi'] })
+    ),
+    R.over(
+      R.lensProp('properties'),
+      R.pipe(R.append('openFile'), R.append('multiSelections'))
+    )
   )(options);
 
   console.log(newOpts);
